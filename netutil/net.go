f5096df95a4e5329903cb5ba5a47ccc038dfcf9d
roman <roman@MacBook-Pro-roman.local>
2017-07-28 10:58:55 +0200

    add ip2long and ip range to cidr

--- netutil/net.go
+++ netutil/net.go
@@ -3,6 +3,7 @@ package netutil
 import (
 	"net"
 	"strings"
+	"encoding/binary"
 )
 
 func ParseIP(addr string) net.IP {
@@ -21,4 +22,33 @@ func MustParseCIDR(s string) *net.IPNet {
 	}
 
 	return ipnet
+}
+
+func IpToInt(ip net.IP) uint32 {
+	if len(ip) == 16 {
+		return binary.BigEndian.Uint32(ip[12:16])
+	}
+	return binary.BigEndian.Uint32(ip)
+}
+
+func IntToIp(n uint32) net.IP {
+	ip := make(net.IP, 4)
+	binary.BigEndian.PutUint32(ip, n)
+	return ip
+}
+
+func IpRangeToCIDR(ipFrom net.IP, ipTo net.IP) *net.IPNet {
+	max := 32
+	for l := max; l >= 0; l-- {
+		mask := net.CIDRMask(l, max)
+
+		na := ipFrom.Mask(mask)
+		n := net.IPNet{IP: na, Mask: mask}
+
+		if n.Contains(ipTo) {
+			return &n
+		}
+	}
+
+	return nil
 }
