df27032f88044c919178eddbbab322dae4599370
sintanial <sintanial@gmail.com>
2019-12-26 11:51:44 +0300

    add api

---
+++ httputil/api.go
@@ -0,0 +1,50 @@
+package httputil
+
+import (
+	"github.com/ansel1/merry"
+	"github.com/sintanial/go-lerry"
+	"net/http"
+)
+
+type ApiResponse struct {
+	Status bool        `json:"status"`
+	Reason string      `json:"reason"`
+	Data   interface{} `json:"data"`
+}
+
+func SuccessApiResponse(data interface{}) ApiResponse {
+	return ApiResponse{true, "", data}
+}
+
+func WriteSuccessApiResponse(w http.ResponseWriter, r *http.Request, data interface{}) error {
+	return WriteJson(SuccessApiResponse(data), w, r)
+}
+
+func FailedApiResponse(reason string) ApiResponse {
+	return ApiResponse{false, reason, nil}
+}
+
+func WriteFailedApiResponse(w http.ResponseWriter, r *http.Request, reason string) error {
+	return WriteJson(FailedApiResponse(reason), w, r)
+}
+
+type ApiHandler func(w http.ResponseWriter, r *http.Request) (interface{}, error)
+
+func WrapApiHandler(handler ApiHandler) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		data, err := handler(writer, request)
+		if err == nil {
+			SuccessApiResponse(data)
+			return
+		}
+
+		msg := merry.UserMessage(err)
+		if msg == "" {
+			msg = merry.Message(err)
+		}
+
+		FailedApiResponse(msg)
+
+		lerry.Log(err)
+	}
+}
